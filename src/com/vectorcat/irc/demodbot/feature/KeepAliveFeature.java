6cc3b0744bce3ecc1091cfe3512312e8104af451
demodude4u <demodude4u@gmail.com>
2014-02-15 17:19:24 -0500

    Fixed Keep Alive to rejoin channels too

--- src/com/vectorcat/irc/demodbot/feature/KeepAliveFeature.java
+++ src/com/vectorcat/irc/demodbot/feature/KeepAliveFeature.java
@@ -1,7 +1,9 @@
 package com.vectorcat.irc.demodbot.feature;
 
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
+import com.google.common.collect.Sets;
 import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
 import com.google.common.util.concurrent.AbstractExecutionThreadService;
@@ -9,22 +11,30 @@ import com.google.common.util.concurrent.Service;
 import com.google.common.util.concurrent.Uninterruptibles;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import com.vectorcat.irc.Channel;
 import com.vectorcat.irc.IRCControl;
+import com.vectorcat.irc.IRCState;
 import com.vectorcat.irc.demodbot.event.help.FeatureRollCall;
 import com.vectorcat.irc.event.IRCServerDisconnect;
 import com.vectorcat.irc.event.IRCServerExceptionEvent;
+import com.vectorcat.irc.event.recv.IRCRecvJoin;
+import com.vectorcat.irc.event.recv.IRCRecvPart;
 
 @Singleton
 public class KeepAliveFeature {
 
 	private final IRCControl control;
+	private final IRCState state;
 
 	private volatile Service reconnectService = null;
 
+	private final Set<Channel> channels = Sets.newLinkedHashSet();
+
 	@Inject
 	public KeepAliveFeature(HelpFeature helpFeature, EventBus bus,
-			IRCControl control) {
+			IRCControl control, IRCState state) {
 		this.control = control;
+		this.state = state;
 
 		bus.register(this);
 	}
@@ -43,6 +53,15 @@ public class KeepAliveFeature {
 							try {
 								control.reconnectServer();
 								System.out.println("Connected!");
+								for (Channel channel : channels) {
+									try {
+										channel.join();
+									} catch (Exception e) {
+										System.err.println("Error joining "
+												+ channel + " : "
+												+ e.getClass().getSimpleName());
+									}
+								}
 							} catch (Exception e) {
 								System.err
 										.println(e.getClass().getSimpleName());
@@ -70,6 +89,20 @@ public class KeepAliveFeature {
 	@Subscribe
 	public void onIRCServerExceptionEvent(IRCServerExceptionEvent event) {
 		checkOrReconnect();
+	}
+
+	@Subscribe
+	public void onJoin(IRCRecvJoin event) {
+		if (state.getMyUser().equals(event.getUser())) {
+			channels.add(event.getChannel());
+		}
+	}
+
+	@Subscribe
+	public void onPart(IRCRecvPart event) {
+		if (state.getMyUser().equals(event.getUser())) {
+			channels.remove(event.getChannel());
+		}
 	}
 
 }
